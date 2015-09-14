7ca61e620ed9b335d8d9253ee25b7471b5ee8adb
BigBursar <bursar@philspcmods.com>
2015-09-14 16:29:12 +0100

    First upload of OutputContacts and UpdateUsers scripts
    OutputContacts script output mail contacts in Exchange as an HTML file,
    ready for uploading to an Intranet or similar.
    
    UpdateUsers script take a list of users from a CSV file (template
    included), looks for them in AD, and updates them. It does not create
    missing users.

---
+++ OutputContacts.ps1
@@ -0,0 +1,42 @@
+﻿# Get login credentials to an Exchange server
+$cred = Get-Credential -Message "Log in with valid Username and Password"
+# Update the -ConnectionURI field with the name of an Exchange CAS server
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://<servername>/powershell -Credential $cred
+Import-PSSession $session
+
+# Import all active AD Users
+## Update the variable below with the domain to look for e.g. '*contoso*'
+$ADUsers = Get-MailContact -resultsize unlimited -Filter { ExternalEmailAddress -like '*contoso*' } -SortBy Name
+
+# Create HTML Table
+$HTML = "<table><tr><td><strong>Name</strong></td><td><strong>E-Mail</strong></td><td><strong>Telephone</strong></td><td><strong>Mobile</strong></td>"
+$HTML+= "<td><strong>Skype</strong></td><td><strong>Title</strong></td><td><strong>Office</strong></td></tr>"
+
+#Loop through each user
+ForEach ($User in $ADUsers) {
+    # Get the required bits of information
+    $email = $User.ExternalEmailAddress.Trim("SMTP:")
+    $Contact = Get-Contact -Identity $email
+    $name = $User.Name
+    $title=$Contact.Title
+    $phone=$Contact.Phone
+    $mobile=$Contact.MobilePhone
+    $skype=$User.CustomAttribute1
+    $office=$Contact.Office
+
+    # Output in HTML format
+    $HTML += "<tr>"
+    $HTML += "<td>$name</td>"
+    $HTML += "<td><a href=""mailto:$email"">$email</a></td>"
+    $HTML += "<td>$phone</td>"
+    $HTML += "<td>$mobile</td>"
+    $HTML += "<td>$skype</td>"
+    $HTML += "<td>$title</td>"
+    $HTML += "<td>$office</td>"
+	$HTML += "</tr>"
+}
+
+$HTML += "</table>"
+
+# Output the results into a file
+$HTML | Out-File "outputs/contacts.html"
---
+++ UpdateUsers.ps1
@@ -0,0 +1,97 @@
+﻿Import-Module ActiveDirectory
+
+# CSV file containing user details
+$ImportedUsers = Import-Csv "inputs\HR-Users-Export.csv"
+
+# Arrays to hold found and missing users
+$FoundUsers = @()
+$MissingUsers = @()
+$NoEmailUsers = @()
+
+[string]$EmailBody = ""
+
+#Loop through each user in the spreadsheet
+ForEach ($ImportedUser in $ImportedUsers)
+{ 
+    $GivenName = $ImportedUser.'Known As'
+    $Surname = $ImportedUser.'Last Name'
+    $Email=$ImportedUser.Email
+    $Manager=$ImportedUser.Manager
+    $DeskNum = $ImportedUser.'Tel. Extension'
+    $Location=$ImportedUser.Location
+    $Department=$ImportedUser.Department
+
+    # If the spreadsheet entry has an email address
+    if (![string]::IsNullOrEmpty($Email)) {
+
+        #Find matching AD account
+        if (Get-ADUser -Filter { Mail -eq $Email  }) {
+            # Get AD account fields
+            $ADUser = Get-ADUser -Filter { Mail -eq $Email  }
+
+            # Check for empty fields and fill with default values
+            if ([string]::IsNullOrEmpty($Department)) { $Department = " " }
+
+            if ([string]::IsNullOrEmpty($Location)) { $Location = " " }
+
+            if ([string]::IsNullOrEmpty($DeskNum)) { 
+                $FormattedDeskNum = "n/a"
+            } else {
+                # Format Desk Phone number
+                $DeskNum=$ImportedUser.'Tel. Extension'.Substring($ImportedUser.'Tel. Extension'.Length-3,3)
+                $FormattedDeskNum = "01954 234 $DeskNum"                
+            }
+    
+            if ([string]::IsNullOrEmpty($ImportedUser.'Work Telephone')) {
+                $FormattedMobileNum = "n/a"
+            } else {
+                # Format Mobile Phone Number
+                $FormattedMobileNum = "{0:+############}" -f $ImportedUser.'Work Telephone'
+            }
+
+            # Update fields
+            # Job Titles in the spreadsheet are currently prefixed with garbage data so they are being ignored
+            # Once the job titles in the HR system are correct, uncomment the line below, and comment the line below that
+            # Set-ADUser $ADUser -Description $ImportedUser.'Job Title' -Title $ImportedUser.'Job Title' -Department $Department -Company $Location `
+            Set-ADUser $ADUser -Department $Department -Company $Location `
+                -OfficePhone $FormattedDeskNum -MobilePhone $FormattedMobileNum
+
+            # Get and update Manager
+            if ($ADManager=Get-ADUser -Filter { DisplayName -eq $Manager  }) {
+                Set-ADUser $ADUser -Manager $ADManager.SamAccountName
+            } else {
+            # Manager account doesn't exist, so do nothing
+            }
+
+            # Update list of found users
+            $FoundUsers += ,@($GivenName, $Surname, $Email) 
+        } else {
+            # HR User doesn't exist in AD, so update missing users
+            $MissingUsers += ,@($GivenName, $Surname, $Email) 
+        }
+    } else {
+        # HR User has no email address, so update no email users
+        $NoEmailUsers += ,@($GivenName, $Surname, $Email)
+    }
+}
+
+# Create Report Email
+$EmailBody = "<p><font color = 'green'>The following users were found and updated</font></p>"
+ForEach ($User in ($FoundUsers | Sort-Object)){
+#    Write-Host "$User found and updated" -ForegroundColor "Green"
+    $EmailBody += "$User<br/>"
+}
+$EmailBody += "<hr/><p><font color = 'red'>The following users are in the CSV file, but NOT in AD</font><br/></p>"
+ForEach ($User in ($MissingUsers | Sort-Object)){
+#    Write-Host "$User missing from Active Directory!`r`n" -ForegroundColor "Red"
+    $EmailBody += "$User<br />"
+}
+$EmailBody += "`<hr/><p><font color = 'blue'>The following users have no email address in the CVS file</font></p>"
+ForEach ($User in ($NoEmailUsers | Sort-Object)){
+#    Write-Host "$User has no email address in HR!`r`n" -ForegroundColor "Yellow"
+    $EmailBody += "$User <br />"
+}
+
+# Send report email - update addresses to valid domains!!
+send-mailmessage -to "admin@contoso.com" -from "AD Update Script <adupdatescript@contoso.com>" -subject "AD Update Script Results" -smtpServer mail.contoso.com `
+    -body $EmailBody -BodyAsHtml
